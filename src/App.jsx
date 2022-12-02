65afdb70e3664df2a8107d9fccbc69aa611bd632
Matias Arias <matt.arias182@gmail.com>
2022-12-02 15:12:30 -0300

    se agrego rutas con react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import Home from "./pages/Home";
+import Login from "./components/Login";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Container, Box } from "@mui/material";
+
+const Login = () => {
+  return <Container maxWidth="lg">Login</Container>;
+};
+
+export default Login;
